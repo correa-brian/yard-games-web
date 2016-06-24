aa6182e82a1e8f997d98855c6ea3dc714abab3c0
Brian <briancorrea@gmail.com>
2016-06-24 14:17:29 -0400

    Using Account Route

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/api', api);
+app.use('/account', account);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
